77449bdd4399810acf1930f318d6c416eacb05f5
RuthRose1 <124596477+RuthRose1@users.noreply.github.com>
2023-10-16 08:37:35 +0530

    Delete Day_4 directory

--- Day_4/Program 4.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-response = requests.get("https://news.ycombinator.com/") #link
-print(response) #o/p -> response -> 200 ie,status success
-
-print(response.text)
